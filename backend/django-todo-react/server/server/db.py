1a01ff3abb48a52ca9c13601534582059b4a8f2c
Chaim Ochs <chaimochs@gmail.com>
2020-07-12 21:23:05 +0300

    hid django p/w

---
+++ backend/django-todo-react/server/server/db.py
@@ -0,0 +1,1 @@
+'@%s1ehi1rz=qo+y0vs8paia=5lw-_yycs^g+n2j-re-+s*r-$('
--- backend/django-todo-react/server/server/settings.py
+++ backend/django-todo-react/server/server/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@%s1ehi1rz=qo+y0vs8paia=5lw-_yycs^g+n2j-re-+s*r-$('
+f = open("db.py", "r")
+SECRET_KEY = f.read()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
